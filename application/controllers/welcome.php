ec5bfc80fcb6c5dab0847b9b5acf7894aec73c22
Amanda <Amanda@Amanda-VAIO>
2012-10-13 11:25:59 -0300

    Váriaveis inseridas em class View

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,11 +1,16 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Welcome extends MY_Controller {
-    
+
+    public function __construct() {
+        parent::__construct();
+    }
+
     public function index() {
-        //$this->load->view('welcome_message');
-        $this->sVAR['teste'] = 'xxxx';
-        $this->view->teste();
+        $this->load->view('welcome_message');
     }
 
 }
--- application/libraries/view.php
+++ application/libraries/view.php
@@ -1,23 +1,24 @@
 <?php
 
 /**
- *
  * @author luiz.vinicius73@gmail.com
  */
 class View {
+    #Variavel das views
 
     protected $sVAR = array();
+    #Views a serem carregadas
+    protected $setView = array('HEADER' => 'comum/head', 'FOOTER' => 'comum/footer', 'PAGE' => NULL);
+    #Template do Site
+    private $TEMPLATE = NULL;
+    # @var CodeIgiter
     private $CI;
 
     public function __construct() {
+        #Instancia Objeto
         $this->CI = &get_instance();
+        #sVAR
         $this->sVAR = &$this->CI->sVAR;
-    }
-    
-    public function teste(){
-        print_r($this->sVAR);
     }
 
 }
-
-?>
